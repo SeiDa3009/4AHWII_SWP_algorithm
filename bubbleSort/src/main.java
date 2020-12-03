d834663777c90c182fd27d537f59e6d6a26ab432
SeiDa3009 <daseidl@tsn.at>
2020-12-03 09:00:30 +0100

    bubblesort

--- bubbleSort/src/main.java
+++ bubbleSort/src/main.java
@@ -2,15 +2,20 @@ import models.*;
 import java.lang.*;
 public class main {
     public static void main(String[] args) {
-        long time = System.currentTimeMillis();
         bubbleSortMethods m1 = new bubbleSortMethods();
         int[] numbers = m1.createRandomArray(m1.ArrayLength());
+        long time = System.currentTimeMillis();
+
         m1.sortArray(numbers);
+        System.out.println("Hat Millisekunden gebraucht: " + (System.currentTimeMillis()-time));
+        System.out.println("Hat Sekunden gebraucht: " + ((System.currentTimeMillis()-time)/1000));
+
+        //Ausgabe
         for(int i = 0; i < numbers.length; i++){
             //System.out.println(m1.sortArray(numbers)[i]);
         }
-        System.out.println("Hat Millisekunden gebraucht: " + (System.currentTimeMillis()-time));
-        System.out.println("Hat Sekunden gebraucht: " + ((System.currentTimeMillis()-time)/1000));
+        m1.getCount();
+
 
     }
 }
--- bubbleSort/src/models/bubbleSortMethods.java
+++ bubbleSort/src/models/bubbleSortMethods.java
@@ -3,18 +3,20 @@ package models;
 
 import java.util.Random;
 import java.util.Scanner;
-
 public class bubbleSortMethods {
+    static int count = 0;
 
 
-    public int[] sortArray(int[] numbers){
+    public int[] sortArray(int[] numbers){                                              //Bubble sort method
         int c;
+
         for(int i = 1;i < numbers.length; i++){
             for(int k = 0; k < (numbers.length-i); k++){
                 if(numbers[k] > numbers[k+1]){
                     c = numbers[k];
                     numbers[k] = numbers[k+1];
                     numbers[k+1] = c;
+                    count++;                                                            //change counter
                 }
             }
         }
@@ -23,15 +25,18 @@ public class bubbleSortMethods {
 
     public int ArrayLength(){
         Scanner reader = new Scanner(System.in);
-        System.out.print("Geben Sie bitte einen Wert für die Arraylänge ein: ");
+        System.out.print("Geben Sie bitte einen Wert für die Arraylänge ein: ");        //set array length
         return reader.nextInt();
     }
-    public int[] createRandomArray(int arrayLength){
+    public int[] createRandomArray(int arrayLength){                                    //creating random numbers in the array
         Random rd = new Random();
         int[] numbers = new int[arrayLength];
         for(int i = 0; i < numbers.length; i++){
             numbers[i] = rd.nextInt();
         }
         return numbers;
+    }
+    public void getCount(){
+         System.out.println(count);
     }
 }
